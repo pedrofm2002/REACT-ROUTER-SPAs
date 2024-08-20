dd3562b088f9d6ee035ce6369ea8b95071ad418f
pedrof2002 <pedroferrarimartinez12@gmail.com>
2024-08-20 14:27:08 -0300

    SPA Router

---
+++ src/router.jsx
@@ -0,0 +1,34 @@
+import { createBrowserRouter } from "react-router-dom"
+import Admin from "./pages/admin/Admin";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Cart from "./pages/Cart";
+import RootLayout from "./pages/RootLayout";
+import Product from "./pages/Product";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [{
+        index: true,
+        element: <Home />
+    }, {
+        path: "/products",
+        element: <Products />,
+      }, {
+        path: "products/:productId",
+        element: <Product />
+
+      }, {
+        path: "/cart",
+        element: <Cart />,
+    }]
+},
+  {
+    path: "/admin",
+    element: <Admin />,
+  }
+])
+
+export default router;
